f18f9f80d7deb6e1f731f1a2824d005337fcacd1
ZANFRAN13 <fzanbisignani@udesa.edu.ar>
2024-06-20 17:49:55 -0300

    varios fail to commit juntos x autosave desactivado

--- src/Navs/NavTab.js
+++ src/Navs/NavTab.js
@@ -1,7 +1,8 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Component } from 'react';
 import homeScreen from '../screens/HomeScreen/homeScreen';
-import newPost from '../screens/newPost/newPost';
+// import newPost from '../screens/newPost/newPost';
+import nuevoPosteo from '../screens/nuevoPosteo/nuevoPosteo';
 import Profile from '../screens/Profile/Profile';
 import {FontAwesome} from '@expo/vector-icons';
 import { MaterialIcons } from '@expo/vector-icons';
@@ -19,7 +20,8 @@ class HomeMenu extends Component{
         return (
             <Tab.Navigator screenOptions={{tabBarShowLabel: false }}>
                 <Tab.Screen name="Home" component={homeScreen}  options={{tabBarIcon: () => <FontAwesome name="home" size={24} color="black" />}}/>
-                <Tab.Screen name="New Post" component={newPost}  options={{tabBarIcon: () => <AntDesign name="plussquare" size={24} color="black" />}} />
+                {/* <Tab.Screen name="New Post" component={newPost}  options={{tabBarIcon: () => <AntDesign name="plussquare" size={24} color="black" />}} /> */}
+                <Tab.Screen name="Nuevo Posteo" component={nuevoPosteo}  options={{tabBarIcon: () => <FontAwesome name="nuevoPosteo" size={24} color="black" />}}/>
                 <Tab.Screen name="Profile" component={Profile}  options={{tabBarIcon: () => <MaterialIcons name="my-library-books" size={24} color="black" />}} />
             </Tab.Navigator>
         )
--- src/screens/HomeScreen/homeScreen.js
+++ src/screens/HomeScreen/homeScreen.js
@@ -1,7 +1,105 @@
-import React from 'react'
+import react, { Component } from 'react';
+import { FontAwesome } from '@expo/vector-icons';
+import {TextInput, TouchableOpacity, View, Text, StyleSheet, ActivityIndicator, FlatList} from 'react-native';
+import { db, auth } from '../../config/config';
+// import Post from '../../components/Post';
+// import PostDos from '../../components/postDos'
 
-export default function homeScreen() {
-  return (
-    <div>homeScreen</div>
-  )
+class homeScreen extends Component {
+    constructor(){
+        super()
+        this.state={
+            postList:[],
+            loader: true
+        }
+    }
+    componentDidMount(){
+        db.collection('posts').onSnapshot(
+            docs => {
+                let postsShown = [];
+
+                docs.forEach(posteo=>{
+                    postsShown.push({
+                        id: posteo.id,
+                        data: posteo.data()})
+                })
+            this.setState({ postList:postsShown, loader: false })
+            })
+    }
+
+    render(){
+        console.log('current user;',auth.currentUser)
+        return(
+          <View>
+            { this.state.loader === true ? <ActivityIndicator  size='large' color='gray'/>
+            :
+                <View style={styles.generalContainer}>
+                {console.log('estoy en homeScreen')}
+                <Text style={styles.title}>Coffeegram</Text>
+
+                <FlatList
+                    data={this.state.postList}
+                    keyExtractor={posteo => posteo.id.toString()}
+                    renderItem={({item})=><PostDos propsNav={this.props} postInfo={item} style={styles.element} />}
+                    // thispropsnavigation
+                />
+            
+                </View>
+            }
+          </View>
+        )
+    }
 }
+
+const styles = StyleSheet.create({
+    generalContainer:{
+        flex: 1,
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        flexWrap: 'wrap',
+        flexShrink: '0'
+    },
+    title:{
+         color: '#443742',
+         fontWeight: 'bold',
+         fontSize: 40,
+    },
+    // flatList:{
+    // },
+    element:{
+        display:'flex',
+        flexDirection:'column',
+        margin: 30,
+        padding: 30,
+        justifyContent: 'space-around',
+    },
+    input:{
+        height:20,
+        paddingVertical:15,
+        paddingHorizontal: 10,
+        borderWidth:1,
+        borderColor: '#ccc',
+        borderStyle: 'solid',
+        borderRadius: 6,
+        marginVertical:10,
+    },
+    button:{
+        backgroundColor:'#28a745',
+        paddingHorizontal: 10,
+        paddingVertical: 6,
+        textAlign: 'center',
+        borderRadius:4,
+        borderWidth:1,
+        borderStyle: 'solid',
+        borderColor: '#28a745'
+    },
+    textButton:{
+        color: '#fff'
+    }
+
+})
+
+
+export default homeScreen;
