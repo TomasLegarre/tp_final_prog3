37a327d0f8cfd481c7e32acb4d0bbd88d7bc8486
tomaslegarre <tlegarrematera@udesa.edu.ar>
2024-06-21 16:18:48 -0300

    listo likes y estilos

--- src/Componentes/Posteo.js
+++ src/Componentes/Posteo.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet, Image, FlatList } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, Image } from 'react-native';
 import { auth, db } from '../config/config';
 import firebase from 'firebase';
-
 
 class Posteo extends Component {
   constructor() {
@@ -54,7 +53,7 @@ class Posteo extends Component {
   }
 
   render() {
-    console.log(this.props) // veo que me trae props asi pongo bien los nombres de cada cosa (img, descrpcion, etc)
+    console.log(this.props); // veo que me trae props asi pongo bien los nombres de cada cosa (img, descrpcion, etc)
     return (
       <View style={styles.container}>
         <Image
@@ -67,95 +66,78 @@ class Posteo extends Component {
 
         <Text style={styles.textLike}> {this.state.likes !== undefined ? this.state.likes.length : 0} likes </Text>
 
-
-        {/* SACO O PONGO LIKES DEPENDEINDO SI INCLUYE O NO EL LIKE DEL USUARIO */}
-
-        {this.state.likes.includes(auth.currentUser.email) ? (
-
-          <TouchableOpacity style={styles.button} onPress={() => this.desLike()}>
-            <Text style={styles.buttonText}>Sacar like</Text>
-          </TouchableOpacity>
-
-        ) : (
-
-          <TouchableOpacity style={styles.button} onPress={() => this.Like()}>
-            <Text style={styles.buttonText}>Dar like</Text>
-          </TouchableOpacity>
-        )}
-
-        {/* ESTO TE REDIREIGE AL USUARIO. si tocas profile te manda a profile. propio o ajeno */}
-
-        <TouchableOpacity style={styles.button} onPress={()=>this.props.navigation.navigate('Profile', {mail: this.props.postInfo.data.owner})}>
-          <Text style={styles.buttonText}>Nombre de usuario : {this.props.postInfo.data.owner}</Text>
-        </TouchableOpacity>
-
-        {/* <TouchableOpacity
-          style={styles.button}
-          onPress={() => this.props.navigation.navigate('Comments', { info: this.props.postInfo })}
-        >
-          <Text style={styles.buttonText}>
-            Cantidad de comentarios: {this.state.comentarios.length}
-          </Text>
-        </TouchableOpacity> */}
-
-      
-        {/* <FlatList
-          data={this.state.comentarios.slice(0, 4)} 
-          keyExtractor={(item, index) => `${index}_${item.fecha}`} 
-          renderItem={({ item }) => (
-            <View style={styles.comentarioContainer}>
-              <Text style={styles.comentarioUsuario}>{item.usuario}</Text>
-              <Text style={styles.comentarioTexto}>{item.comentario}</Text>
-            </View>
+        <View style={styles.buttonContainer}>
+          {this.state.likes.includes(auth.currentUser.email) ? (
+            <TouchableOpacity style={styles.button} onPress={() => this.desLike()}>
+              <Text style={styles.buttonText}>Sacar like</Text>
+            </TouchableOpacity>
+          ) : (
+            <TouchableOpacity style={styles.button} onPress={() => this.Like()}>
+              <Text style={styles.buttonText}>Dar like</Text>
+            </TouchableOpacity>
           )}
-        /> */}
 
+          <TouchableOpacity style={styles.button} onPress={() => this.props.navigation.navigate('Profile', { mail: this.props.postInfo.data.owner })}>
+            <Text style={styles.buttonText}>Usuario: {this.props.postInfo.data.owner}</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
 }
-
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'black', 
+    backgroundColor: '#141414', // Fondo negro
+    marginVertical: 10,
+    padding: 10,
+    borderRadius: 8,
   },
   foto: {
     height: 300,
-    width: '100%',
+    width: '30%', // Reduce el ancho de la imagen
     borderRadius: 8,
+    marginBottom: 10,
   },
   text: {
-    color: '#FFEBEE',
-    fontSize: 18,
-    marginBottom: 10,
+    color: '#FFFFFF',
+    fontSize: 16,
+    marginBottom: 5,
+    textAlign: 'center',
   },
   textLike: {
-    color: '#FFEBEE',
-    fontSize: 18,
+    color: '#B0B0B0',
+    fontSize: 14,
     marginBottom: 10,
+    textAlign: 'center',
+  },
+  buttonContainer: {
+    width: '80%', // Ajustar el ancho del contenedor de botones al ancho de la foto
   },
   button: {
-    backgroundColor: '#D32F2F',
-    padding: 10,
+    backgroundColor: '#E50914',
+    paddingVertical: 10, // Ajustar padding vertical
     borderRadius: 8,
     marginVertical: 5,
+    width: '100%', // Hacer que el botón ocupe todo el ancho del contenedor de botones
   },
   buttonText: {
-    color: '#FFEBEE',
+    color: '#FFFFFF',
     fontWeight: 'bold',
+    textAlign: 'center',
+    fontSize: 14, // Ajustar el tamaño del texto
   },
   comentarioContainer: {
-    backgroundColor: '#333', 
+    backgroundColor: '#333333', 
     padding: 8,
     marginVertical: 4,
     borderRadius: 8,
   },
   comentarioUsuario: {
-    color: '#D32F2F',
+    color: '#E50914',
     fontWeight: 'bold',
   },
   comentarioTexto: {
