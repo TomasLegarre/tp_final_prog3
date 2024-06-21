eb89731eb7c7eb43c766c28799d22a579d57efdd
tomaslegarre <tlegarrematera@udesa.edu.ar>
2024-06-21 15:55:20 -0300

    va quedadno el postssss

--- src/Componentes/Posteo.js
+++ src/Componentes/Posteo.js
@@ -1,7 +1,166 @@
-import React from 'react'
+import React, { Component } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet, Image, FlatList } from 'react-native';
+import { auth, db } from '../config/config';
+import firebase from 'firebase';
 
-export default function Posteo() {
-  return (
-    <div>Posteo</div>
-  )
+
+class Posteo extends Component {
+  constructor() {
+    super();
+    this.state = {
+      likes: [],
+      comentarios: [],
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      likes: this.props.postInfo.data.likes || [],
+      comentarios: this.props.postInfo.data.comentarios || [],
+    });
+  }
+
+  Like() {
+    db.collection('posts')
+      .doc(this.props.postInfo.id)
+      .update({
+        likes: firebase.firestore.FieldValue.arrayUnion(auth.currentUser.email),
+      })
+      .then(() => {
+        let Array = this.state.likes;
+        Array.push(auth.currentUser.email);
+        this.setState({
+          likes: Array,
+        });
+      })
+      .catch(e => console.log(e));
+  }
+
+  desLike() {
+    db.collection('posts')
+      .doc(this.props.postInfo.id)
+      .update({
+        likes: firebase.firestore.FieldValue.arrayRemove(auth.currentUser.email),
+      })
+      .then(() => {
+        let Array = this.state.likes;
+        let ArrayFiltrado = Array.filter(usuario => usuario !== auth.currentUser.email);
+
+        this.setState({
+          likes: ArrayFiltrado,
+        });
+      })
+      .catch(e => console.log(e));
+  }
+
+  render() {
+    console.log(this.props) // veo que me trae props asi pongo bien los nombres de cada cosa (img, descrpcion, etc)
+    return (
+      <View style={styles.container}>
+        <Image
+          style={styles.foto}
+          source={{ uri: this.props.postInfo.data.imageURL }} // aca me parece que va ImageUrl --> como esta en firebase
+          resizeMode='cover'
+        />
+
+        <Text style={styles.text}>{this.props.postInfo.data.post}</Text>
+
+        <Text style={styles.textLike}> {this.state.likes !== undefined ? this.state.likes.length : 0} likes </Text>
+
+
+        {/* SACO O PONGO LIKES DEPENDEINDO SI INCLUYE O NO EL LIKE DEL USUARIO */}
+
+        {this.state.likes.includes(auth.currentUser.email) ? (
+
+          <TouchableOpacity style={styles.button} onPress={() => this.desLike()}>
+            <Text style={styles.buttonText}>Sacar like</Text>
+          </TouchableOpacity>
+
+        ) : (
+
+          <TouchableOpacity style={styles.button} onPress={() => this.Like()}>
+            <Text style={styles.buttonText}>Dar like</Text>
+          </TouchableOpacity>
+        )}
+
+        {/* ESTO TE REDIREIGE AL USUARIO. si tocas profile te manda a profile. propio o ajeno */}
+
+        <TouchableOpacity style={styles.button} onPress={()=>this.props.navigation.navigate('Profile', {mail: this.props.postInfo.data.owner})}>
+          <Text style={styles.buttonText}>Nombre de usuario : {this.props.postInfo.data.owner}</Text>
+        </TouchableOpacity>
+
+        {/* <TouchableOpacity
+          style={styles.button}
+          onPress={() => this.props.navigation.navigate('Comments', { info: this.props.postInfo })}
+        >
+          <Text style={styles.buttonText}>
+            Cantidad de comentarios: {this.state.comentarios.length}
+          </Text>
+        </TouchableOpacity> */}
+
+      
+        {/* <FlatList
+          data={this.state.comentarios.slice(0, 4)} 
+          keyExtractor={(item, index) => `${index}_${item.fecha}`} 
+          renderItem={({ item }) => (
+            <View style={styles.comentarioContainer}>
+              <Text style={styles.comentarioUsuario}>{item.usuario}</Text>
+              <Text style={styles.comentarioTexto}>{item.comentario}</Text>
+            </View>
+          )}
+        /> */}
+
+      </View>
+    );
+  }
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'black', 
+  },
+  foto: {
+    height: 300,
+    width: '100%',
+    borderRadius: 8,
+  },
+  text: {
+    color: '#FFEBEE',
+    fontSize: 18,
+    marginBottom: 10,
+  },
+  textLike: {
+    color: '#FFEBEE',
+    fontSize: 18,
+    marginBottom: 10,
+  },
+  button: {
+    backgroundColor: '#D32F2F',
+    padding: 10,
+    borderRadius: 8,
+    marginVertical: 5,
+  },
+  buttonText: {
+    color: '#FFEBEE',
+    fontWeight: 'bold',
+  },
+  comentarioContainer: {
+    backgroundColor: '#333', 
+    padding: 8,
+    marginVertical: 4,
+    borderRadius: 8,
+  },
+  comentarioUsuario: {
+    color: '#D32F2F',
+    fontWeight: 'bold',
+  },
+  comentarioTexto: {
+    color: '#FFEBEE',
+  },
+});
+
+export default Posteo;
--- src/Navs/NavTab.js
+++ src/Navs/NavTab.js
@@ -1,11 +1,8 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Component } from 'react';
-
 import nuevoPosteo from '../screens/nuevoPosteo/nuevoPosteo';
 import Profile from '../screens/Profile/Profile';
 import {FontAwesome} from '@expo/vector-icons';
-
-
 import { MaterialIcons } from '@expo/vector-icons';
 import StackSecundaria from './StackSecundaria';
 
@@ -21,11 +18,11 @@ class HomeMenu extends Component{
         return (
             <Tab.Navigator screenOptions={{tabBarShowLabel: false }}>
                 <Tab.Screen name="Home" component={StackSecundaria}  options={{tabBarIcon: () => <FontAwesome name="home" size={24} color="black" />}}/>
-                <Tab.Screen name="Nuevo Posteo" component={nuevoPosteo}  options={{tabBarIcon: () => <FontAwesome name="nuevoPosteo" size={24} color="black" />}}/>
+                <Tab.Screen name="Nuevo Posteo" component={nuevoPosteo}  options={{tabBarIcon: () => <MaterialIcons name="post-add" size={24} color="black" />}}/>
                 <Tab.Screen name="Profile" component={Profile}  options={{tabBarIcon: () => <MaterialIcons name="my-library-books" size={24} color="black" />}} />
-            </Tab.Navigator>
+            </Tab.Navigator> 
         )
     }
 }
 
-export default HomeMenu;
+export default HomeMenu; 
